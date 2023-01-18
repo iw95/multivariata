1c687d49881ef45ff15bfffe20f52e8d7866da50
iw95 <leorumi@mnet-online.de>
2023-01-18 18:39:31 +0100

    implemented kmeans + running gmm with starting parameters computed by kmeans

---
+++ gmm/kmeans.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+class Kmeans:
+    def __init__(self, data, k, iterations=5):
+        self.data = data
+        self.datashape = data.shape
+        self.k = k
+        # initialize
+        rng = np.random.default_rng(12345)
+        self.labels = np.zeros((self.datashape[0]))
+        self.means = rng.choice(data, k, replace=False)
+        # clustering for <iterations> iterations
+        self.cluster(iterations)
+        # return labels and means
+        return
+
+    def cluster(self, iterations):
+        dist = np.zeros((self.datashape[0],self.k))
+        new_means = np.zeros((self.k,self.datashape[1]))
+        for i in range(iterations):
+            # recompute labels
+            for k in range(self.k):
+                dist[:,k] = np.linalg.norm(self.data - np.outer(np.ones((self.datashape[0],1)), self.means[k]), axis=1)
+            self.labels = np.argmin(dist, axis=1)
+            # visualize
+            self.visualize(i)
+            # recompute means
+            for k in range(self.k):
+                la_k = self.labels==k
+                new_means[k] = np.sum(self.data[la_k], axis=0) / np.sum(la_k)
+            # compute change between means
+            change = np.abs(new_means-self.means)
+            self.means = new_means
+        return
+
+    def visualize(self, iteration):
+        colours = mcolors.BASE_COLORS
+        col_iter = iter(colours)
+        dim1, dim2 = 0, 1
+        fig = plt.figure()
+        for k in range(self.k):
+            c = next(col_iter)
+            kdata = self.data[self.labels==k]
+            plt.plot(kdata[:,dim1], kdata[:,dim2], c+'.')
+            plt.plot(self.means[k,dim1], self.means[k,dim2], c+'o')
+            # save figure
+        plt.savefig(f'img/kmeans_iter{iteration}.png')
--- gmm/run_gmm.py
+++ gmm/run_gmm.py
@@ -1,15 +1,35 @@
 import numpy as np
 from scipy.stats import multivariate_normal
 import GMM
+import kmeans
 
+
+def normal_data():
+    # generating multivariate data
+    mnd1 = multivariate_normal(np.array([0, 0]), np.array([[2, 0], [0, 3]]))
+    mnd2 = multivariate_normal(np.array([1, 2]), np.array([[1, 0.75], [0.75, 2]]))
+    data = np.concatenate((mnd1.rvs(15), mnd2.rvs(10)), axis=0)
+    return data
+
+
+def gmm():
+    data = normal_data()
+    clustering = GMM.GMM(data, 2, threshold=0.1)
+
+
+def kmeaning():
+    data = normal_data()
+    km = kmeans.Kmeans(data, 2, iterations=3)
+
+def gmm_on_kmeans():
+    k = 2
+    data = normal_data()
+    km = kmeans.Kmeans(data,k,iterations=3)
+    probs = np.concatenate([np.expand_dims(np.array(km.labels==i, dtype=float),axis=1) for i in range(k)],axis=1)
+    gmm = GMM.GMM(data, k=k, threshold=0.1, probs=probs)
 
 def main():
-    # generation multivariate data
-    mnd1 = multivariate_normal(np.array([0,0]), np.array([[2,0],[0,3]]))
-    mnd2 = multivariate_normal(np.array([1,2]), np.array([[1,0.75],[0.75,2]]))
-    data = np.concatenate((mnd1.rvs(15),mnd2.rvs(10)),axis=0)
-
-    clustering = GMM.GMM(data, 2, threshold=0.1)
+    gmm_on_kmeans()
 
 
 if __name__ == "__main__":
